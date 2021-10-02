147d0abc11185d6de36530a608747564a15f4e80
alexpeterson30 <91748329+alexpeterson30@users.noreply.github.com>
2021-10-02 10:19:00 +0530

    Create sentitrack.py

---
+++ sentitrack.py
@@ -0,0 +1,12 @@
+import re # for regular expressions
+import pandas as pd 
+pd.set_option("display.max_colwidth", 200)
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import string
+import nltk # for text manipulation
+import warnings 
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+%matplotlib inline
